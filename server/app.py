1e6e93d5f611f3e31d180650082acc63b929da87
Snipy19 <duggughai@gmail.com>
2026-04-10 14:05:23 +0530

    final fix main callable

--- server/app.py
+++ server/app.py
@@ -34,9 +34,10 @@ def step(action: Action):
 def get_state():
     return state
 
+
 def main():
     return app
 
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    main()
