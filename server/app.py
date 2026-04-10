02e3f9002d0357618d97678c3d095f9c5265b294
Snipy19 <duggughai@gmail.com>
2026-04-10 13:45:53 +0530

    fix main callable issue

--- server/app.py
+++ server/app.py
@@ -34,6 +34,9 @@ def step(action: Action):
 def get_state():
     return state
 
-
 def main():
     return app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
